725f92d1b7bb1d3c5f3c00dc0f38948dce7d2f8d
Brian Cavalier <brian@hovercraftstudios.com>
2013-10-30 12:34:55 -0400

    Simplify joinAssimilate and remove Assimilated promise constructor. Optimize flatMap by assuming a truth promise (possibly not valid in the future). Reorganize private methods.
    Add unit tests for algebraic laws for map, ap, and flatMap

--- Promise.js
+++ Promise.js
@@ -17,40 +17,6 @@ function Promise(resolver) {
 	this._fork = resolver;
 }
 
-Promise.prototype._fulfilled = function(resolver) {
-	resolver(this.value);
-};
-
-Promise.prototype._rejected = function(_, reject) {
-	reject(this.value);
-};
-
-var pending = Promise.prototype._resolver = function(resolve, reject) {
-	var self = this;
-
-	function resolvePromise(x) {
-		if(self._resolver !== pending) {
-			return;
-		}
-		self._resolver = self._fulfilled;
-		resolve(self.value = x);
-	}
-
-	function rejectPromise(x) {
-		if(self._resolver !== pending) {
-			return;
-		}
-		self._resolver = self._rejected;
-		reject(self.value = x);
-	}
-
-	try {
-		this._fork(resolvePromise, rejectPromise);
-	} catch(e) {
-		rejectPromise(e);
-	}
-}
-
 Promise.prototype.map = function(f) {
 	var prev = this;
 	return new Promise(function(resolve, reject) {
@@ -70,18 +36,10 @@ Promise.prototype.flatMap = function(f) {
 	var prev = this;
 	return new Promise(function(resolve, reject) {
 		prev._resolver(function(x) {
-			join(f(x), resolve, reject)
+			f(x)._resolver(resolve, reject)
 		}, reject);
 	});
 };
-
-function join(p, resolve, reject) {
-	if(p instanceof Promise) {
-		p.done(resolve, reject);
-	}
-
-	p.then(resolve, reject);
-}
 
 Promise.prototype.flatten = function() {
 	return this.flatMap(identity);
@@ -128,7 +86,7 @@ Promise.prototype.done = function(f, r) {
 		self._resolver(function(x) {
 			runQueue(self, 0, x);
 		}, function(e) {
-			runQueue(self, 1, e, true);
+			runQueue(self, 1, e);
 		});
 	});
 };
@@ -137,7 +95,7 @@ Promise.prototype.then = function(f, r) {
 	var prev = this;
 	return new Promise(function (resolve, reject) {
 		prev._resolver(function (x) {
-			joinFully(prev, x).done(onFulfill, onReject);
+			joinAssimilate(prev, x, onFulfill, onReject);
 		}, onReject);
 
 		function onFulfill(x) {
@@ -158,24 +116,63 @@ Promise.prototype.then = function(f, r) {
 	});
 };
 
-// Coerce x to a promise
-function joinFully(self, x) {
-	if(x === self) {
-		return new Rejected(new TypeError());
+Promise.prototype._fulfilled = function(resolver) {
+	resolver(this.value);
+};
+
+Promise.prototype._rejected = function(_, reject) {
+	reject(this.value);
+};
+
+var pending = Promise.prototype._resolver = function(resolve, reject) {
+	var self = this;
+
+	function resolvePromise(x) {
+		if(self._resolver !== pending) {
+			return;
+		}
+		self._resolver = self._fulfilled;
+		resolve(self.value = x);
 	}
 
-	if(x instanceof Promise) {
-		return x;
+	function rejectPromise(x) {
+		if(self._resolver !== pending) {
+			return;
+		}
+		self._resolver = self._rejected;
+		reject(self.value = x);
 	}
 
 	try {
+		this._fork(resolvePromise, rejectPromise);
+	} catch(e) {
+		rejectPromise(e);
+	}
+}
+
+// Fully traverse x to its ultimate fulfillment value
+function joinAssimilate(self, x, fulfill, reject) {
+	if(x === self) {
+
+		reject(new TypeError());
+
+	} else if(x instanceof Promise) {
+
+		x.done(function(x) {
+			joinAssimilate(self, x, fulfill, reject);
+		}, reject);
+
+	} else {
+
 		var untrustedThen = x === Object(x) && x.then;
 
-		return typeof untrustedThen === 'function'
-			? new Assimilated(x, untrustedThen)
-			: new Fulfilled(x);
-	} catch(e) {
-		return new Rejected(e);
+		if(typeof untrustedThen === 'function') {
+			call(untrustedThen, x, function(value) {
+				joinAssimilate(self, value, fulfill, reject);
+			}, reject);
+		} else {
+			fulfill(x);
+		}
 	}
 }
 
@@ -183,7 +180,7 @@ function addQueue(promise, f, r) {
 	promise._queue ? (promise._queue.push(f, r)) : (promise._queue = [f, r]);
 }
 
-function runQueue(promise, start, value, rethrow) {
+function runQueue(promise, start, value) {
 	var queue = promise._queue;
 	promise._queue = void 0;
 
@@ -192,24 +189,12 @@ function runQueue(promise, start, value, rethrow) {
 			f = queue[i];
 			if(typeof f === 'function') {
 				f(value);
-			} else if(rethrow) {
+			} else if(start) {
 				throw value;
 			}
 		}
 	}
 }
-
-// Assimilate a foreign thenable
-function Assimilated(thenable, untrustedThen) {
-	var self = this;
-	this._resolver = function(resolve, reject) {
-		call(untrustedThen, thenable, function(x) {
-			joinFully(self, x).done(resolve, reject);
-		}, reject);
-	};
-}
-
-Assimilated.prototype = Promise.prototype;
 
 // A fulfilled promise
 // NOTE: Must not be exposed
@@ -238,7 +223,7 @@ function of(x) {
 // If x is a trusted promise, return it, otherwise
 // return a new promise that follows x
 function cast(x) {
-	return x instanceof Promise ? x : of(x);
+	return x instanceof Promise ? x : of(x).then();
 }
 
 // Return a promise that is rejected with reason x
@@ -261,7 +246,7 @@ function all(array) {
 
 		results = [];
 		array.forEach(function(item, i) {
-			of(item).then(identity).done(function(value) {
+			item.done(function(value) {
 				results[i] = value;
 
 				if(!--toResolve) {
--- test/Promise-test.js
+++ test/Promise-test.js
@@ -7,7 +7,25 @@ var Promise = require('../Promise');
 var sentinel = { value: 'sentinel' };
 var other = { value: 'other' };
 
+function assertp(assert, done, p1, p2) {
+	p1.done(function(x) {
+		p2.done(function(y) {
+			assert(x, y);
+			done();
+		});
+	});
+}
+
+function assertEquals(done, p1, p2) {
+	assertp(assert.equals, done, p1, p2);
+}
+
+function assertSame(done, p1, p2) {
+	assertp(assert.same, done, p1, p2);
+}
+
 buster.testCase('Promise', {
+
 	'done': {
 		'should initiate resolver': function(done) {
 			new Promise(function(resolve, reject) {
@@ -15,6 +33,96 @@ buster.testCase('Promise', {
 				assert.isFunction(reject);
 				done()
 			}).done();
+		}
+	},
+
+	'of': {
+		'should create a promise for x': function(done) {
+			Promise.of(sentinel).done(function(x) {
+				assert.same(x, sentinel);
+				done();
+			});
+		}
+	},
+
+	'map': {
+		'should satisfy identity': function(done) {
+			// u.map(function(a) { return a; })) ~= u
+			var u = Promise.of(sentinel);
+			assertSame(done, u.map(function(x) { return x; }), u);
+		},
+
+		'should satisfy composition': function(done) {
+			//u.map(function(x) { return f(g(x)); }) ~= u.map(g).map(f)
+			function f(x) { return x + 'f'; }
+			function g(x) { return x + 'g'; }
+
+			var u = Promise.of('e');
+
+			assertEquals(done,
+				u.map(function(x) { return f(g(x)); }),
+				u.map(g).map(f)
+			);
+		}
+	},
+
+	'flatMap': {
+		'should satisfy associativity': function(done) {
+			// m.flatMap(f).flatMap(g) ~= m.flatMap(function(x) { return f(x).flatMap(g); })
+			function f(x) { return Promise.of(x + 'f'); }
+			function g(x) { return Promise.of(x + 'g'); }
+
+			var m = Promise.of('m');
+
+			assertEquals(done,
+				m.flatMap(function(x) { return f(x).flatMap(g); }),
+				m.flatMap(f).flatMap(g)
+			);
+		}
+	},
+
+	'ap': {
+		'should satisfy identity': function(done) {
+			// P.of(function(a) { return a; }).ap(v) ~= v
+			var v = Promise.of(sentinel);
+			assertSame(done, Promise.of(function(x) { return x; }).ap(v), v);
+		},
+
+		'should satisfy composition': function(done) {
+			//P.of(function(f) { return function(g) { return function(x) { return f(g(x))}; }; }).ap(u).ap(v).ap(w) ~= u.ap(v.ap(w))
+			var u = Promise.of(function(x) { return 'u' + x; });
+			var v = Promise.of(function(x) { return 'v' + x; });
+			var w = Promise.of('w');
+
+			assertEquals(done, Promise.of(function(f) {
+					return function(g) {
+						return function(x) {
+							return f(g(x));
+						};
+					};
+				}).ap(u).ap(v).ap(w),
+				u.ap(v.ap(w))
+			)
+		},
+
+		'should satisfy homomorphism': function(done) {
+			//P.of(f).ap(P.of(x)) ~= P.of(f(x)) (homomorphism)
+			function f(x) { return x + 'f'; }
+			var x = 'x'
+			assertEquals(done, Promise.of(f).ap(Promise.of(x)), Promise.of(f(x)));
+		},
+
+		'should satisfy interchange': function(done) {
+			// u.ap(a.of(y)) ~= a.of(function(f) { return f(y); }).ap(u)
+			function f(x) { return x + 'f'; }
+
+			var u = Promise.of(f);
+			var y = 'y';
+
+			assertEquals(done,
+				u.ap(Promise.of(y)),
+				Promise.of(function(f) { return f(y); }).ap(u)
+			);
 		}
 	},
 
